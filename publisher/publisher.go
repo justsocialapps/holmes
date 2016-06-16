f708d91ef7f1ef1d983775f6c477c2c1b376e999
Max Jonas Werner <max@just.social>
2016-06-16 10:03:17 +0200

    closes #2 add timestamps to Kafka messages
    - All messages produced by Holmes contain a timestamp now.
    - Holmes now logs successfully delivered messages as well as delivery
      errors.

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -3,6 +3,7 @@ package publisher
 import (
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/shopify/sarama"
 
@@ -11,19 +12,28 @@ import (
 
 func Publish(trackingChannel <-chan *models.TrackingObject, kafkaHost *string, kafkaTopic string) error {
 	kafkaConfig := sarama.NewConfig()
-	kafkaConfig.Producer.Return.Errors = false
+	kafkaConfig.Producer.Return.Errors = true
+	kafkaConfig.Producer.Return.Successes = true
+	kafkaConfig.Producer.RequiredAcks = sarama.WaitForLocal
 	producer, err := sarama.NewAsyncProducer([]string{*kafkaHost}, kafkaConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Kafka producer up and running with broker %s", *kafkaHost)
 
 	var object *models.TrackingObject
 	for {
-		object = <-trackingChannel
-		stringifiedObject, err := json.Marshal(object)
-		if err == nil {
-			log.Printf("publishing %s", stringifiedObject)
-			producer.Input() <- &sarama.ProducerMessage{Topic: kafkaTopic, Key: sarama.StringEncoder("key"), Value: sarama.ByteEncoder(stringifiedObject)}
+		select {
+		case successMsg := <-producer.Successes():
+			log.Printf("successfully delivered msg: %s\n", *successMsg)
+		case errorMsg := <-producer.Errors():
+			log.Printf("error delivering message: %s\n", *errorMsg)
+		case object = <-trackingChannel:
+			stringifiedObject, err := json.Marshal(object)
+			if err == nil {
+				log.Printf("publishing %s", stringifiedObject)
+				producer.Input() <- &sarama.ProducerMessage{Topic: kafkaTopic, Key: sarama.StringEncoder("key"), Value: sarama.ByteEncoder(stringifiedObject), Timestamp: time.Now()}
+			}
 		}
 	}
 }
