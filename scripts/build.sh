770b5e07a44db613f4220b37ac9ea255229c26bf
Max Jonas Werner <max@just.social>
2017-01-20 17:12:45 +0100

    Fixed build script
    - Add .exe extension to windows binaries.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 
-GOOS=windows GOARCH=amd64 go build -o holmes_${VERSION}_windows_amd64
-GOOS=windows GOARCH=386 go build -o holmes_${VERSION}_windows_386
+GOOS=windows GOARCH=amd64 go build -o holmes_${VERSION}_windows_amd64.exe
+GOOS=windows GOARCH=386 go build -o holmes_${VERSION}_windows_386.exe
 GOOS=linux GOARCH=386 go build -o holmes_${VERSION}_linux_386
 GOOS=linux GOARCH=amd64 go build -o holmes_${VERSION}_linux_amd64
