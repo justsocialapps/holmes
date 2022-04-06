135c3aeec5eea60941ff48875c7ea4baaeb31c89
Holger Dewes <holger.dewes@just.social>
2022-04-06 13:16:24 +0200

    Build binary for linux/arm64
    In addition to the currently supported system architectures, build a
    binary for linux with arm64.

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -23,6 +23,7 @@ GOOS=windows GOARCH=amd64 go build -o holmes_${VERSION}_windows_amd64.exe
 GOOS=windows GOARCH=386 go build -o holmes_${VERSION}_windows_386.exe
 GOOS=linux GOARCH=386 go build -o holmes_${VERSION}_linux_386
 GOOS=linux GOARCH=amd64 go build -o holmes_${VERSION}_linux_amd64
+GOOS=linux GOARCH=arm64 go build -o holmes_${VERSION}_linux_arm64
 
 echo Uploading release files...
 for i in holmes_${VERSION}_* ; do
